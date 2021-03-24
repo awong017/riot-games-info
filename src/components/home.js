33ec71ad9be0c65c1b5144bc22357c973c49a511
Adam Wong <adamwthedev@gmail.com>
2021-03-24 12:50:05 -0700

    created fetch in Home component

--- src/components/home.js
+++ src/components/home.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import HomeNav from "./homeNav";
 import TableList from "./tableList";
 import Styled, { ThemeProvider } from "styled-components";
 import globalStyles from "../styles/globalStyles";
 
 const HomeDiv = Styled.div`
-    
+    .error-message {
+        text-align: center;
+        color: red;
+    }
 `
 
 const Home = () => {
+    const [ errorMessage, setErrorMessage ] = useState();
+
+    useEffect(() => {
+        let isSubscribed = true;
+        let apiKey = "RGAPI-a9031d71-ce9e-4621-9d18-984902b71596";
+        let options = {
+            "X-Riot-Token": apiKey
+        }
+        fetch(`https://na.api.riotgames.com/val/content/v1/contents?api_key=${apiKey}`)
+            .then(res => (isSubscribed ? res.json().then(resJson => console.log(resJson)) : null))
+            .catch(error => (isSubscribed ? setErrorMessage(error.toString()) : null))
+        return () => isSubscribed = false
+    }, [])
+
     return (
         <ThemeProvider theme={globalStyles}>
             <HomeDiv>
                 <HomeNav />
                 <TableList />
+                <div className="error-message">{errorMessage}</div>
             </HomeDiv>
         </ThemeProvider>
     );
